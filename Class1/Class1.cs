0ff2829d66d7f6a06e586efa77ca7a627ffb6cea
itsfauzan <fauzanyahya@students.amikom.ac.id>
2023-04-10 21:16:14 +0700

    cmmt4

---
+++ Class1/Class1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Class
+{
+
+    private string warna;
+    private int jumlahPintu;
+    private string merk;
+    private string model;
+    private int tahunKeluar;
+
+
+    public string Warna
+    {
+        get
+        {
+            return warna;
+        }
+        set
+        {
+            warna = value;
+        }
+    }
+
+    public int JumlahPintu
+    {
+        get
+        {
+            return jumlahPintu;
+        }
+        set
+        {
+            jumlahPintu = value;
+        }
+    }
+
+    public string Merk
+    {
+        get
+        {
+            return merk;
+        }
+        set
+        {
+            merk = value;
+        }
+    }
+
+    public string Model
+    {
+        get
+        {
+            return model;
+        }
+        set
+        {
+            model = value;
+        }
+    }
+
+    public int TahunKeluar
+    {
+        get
+        {
+            return tahunKeluar;
+        }
+        set
+        {
+            tahunKeluar = value;
+        }
+    }
+    //method
+    public void Gas(int kecepatan)
+    {
+        Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1}", Model, kecepatan);
+    }
+    public void Klakson(string suara)
+    {
+        Console.WriteLine("suara klakson {0}", suara);
+    }
+
+    public void Tampilkaninfo()
+    {
+        Console.WriteLine("Mobil saya berwarna {0}, merk {1}, model {2}, keluaran tahun {3}, dengan jumlah pintu {4}", Warna, Merk, Model, TahunKeluar, JumlahPintu);
+    }
+
+}
---
+++ Class1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classobj
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Class mobil1 = new Class();
+
+            mobil1.Warna = "Hitam";
+            mobil1.JumlahPintu = 2;
+            mobil1.Merk = "BMW";
+            mobil1.Model = "E30 Coupe";
+            mobil1.TahunKeluar = 1982;
+
+            mobil1.Gas(100);
+            mobil1.Klakson("teeeeet");
+            mobil1.Tampilkaninfo();
+
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+
+}
